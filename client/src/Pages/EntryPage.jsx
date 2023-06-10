c1c6f0a0178d1ea9e4ba93ecd73e307793c88208
Shuhua-L <sl.shuhualiu@gmail.com>
2023-06-10 15:53:52 -0400

    feat: added routes using react-touter and a nav-bar

---
+++ client/src/Pages/EntryPage.jsx
@@ -0,0 +1,12 @@
+
+const EntryPage = () => {
+
+  return (
+    <>
+      <h1>Elder Wand!</h1>
+      <button className='btn btn-accent'>join room</button>
+    </>
+  )
+}
+
+export default EntryPage;
---
+++ client/src/Pages/RoomPage.jsx
@@ -0,0 +1,11 @@
+
+const RoomPage = () => {
+
+  return (
+    <>
+      <h1>This is the room page</h1>
+    </>
+  )
+}
+
+export default RoomPage;
