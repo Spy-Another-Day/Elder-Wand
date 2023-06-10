8c57956873f092ffcf4a07cdadddd0d7d847e769
jason <quintanajason4004@gmail.com>
2023-06-10 19:36:20 -0400

    added some events for the private messaging and slight user authentication

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
+dotenv.config();
 const http = require('http');
 const { Server, Socket } = require('socket.io');
 
@@ -10,48 +11,46 @@ const server = http.createServer(app);
 
 const io = new Server(server, { cors: { origin: '*' } });
 
-// io.use((socket, next) => {
-//   const username = socket.handshake.auth.username;
-//   if (!username) {
-//     return next(new Error('invalid username'));
-//   }
-//   socket.data.username = username;
-//   next();
-// });
-
-
-dotenv.config();
-
-io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`);
-
-  io.emit('test123', 'hello')
-
-  // const users = [];
-  // for (const [id, connectedSocket] of io.of('/').sockets) {
-  //   users.push({
-  //     userID: id,
-  //     username: connectedSocket.data.username,
-  //   });
-  // }
-  // socket.emit('users', users);
-
-  // socket.broadcast.emit('user connected', {
-  //   userID: socket.id,
-  //   username: socket.data.username,
-  // });
-
-  // socket.on('private message', ({ content, to }) => {
-  //   socket.to(to).emit('private message', {
-  //     content: content,
-  //     from: socket.id,
-  //   });
-  // });
-
-  // socket.on('disconnect', () => {
-  //   socket.broadcast.emit('user disconnected', socket.id);
-  // });
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
 });
+
+io.on("connection", (socket) => {
+  // fetch existing users
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  socket.emit("users", users);
+
+  // notify existing users
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.username,
+  });
+
+  // forward the private message to the right recipient
+  socket.on("private message", ({ content, to }) => {
+    socket.to(to).emit("private message", {
+      content,
+      from: socket.id,
+    });
+  });
+
+  // notify users upon disconnection
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user disconnected", socket.id);
+  });
+});
+
 
 // middleware
 app.use(express.json());
