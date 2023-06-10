7b044f1d4bd19ba5c3bb51d17d72292b259a3eb6
Alex Caron <alex.caron@gmail.com>
2023-06-10 15:07:05 -0500

    clean up syntax

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import {ClerkProvider, SignedIn, SignedOut, RedirectToSignIn} from "@clerk/clerk-react";
 const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
@@ -16,4 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </SignedOut>
     </ClerkProvider>
   </React.StrictMode>
-)
+);
