73e75fe0148bdbbb78f1f8b491fc5717ba03aaa0
Alex Caron <alex.caron@gmail.com>
2023-06-12 17:55:24 -0500

    make editing a state

--- client/src/Components/ClueView.jsx
+++ client/src/Components/ClueView.jsx
@@ -1,8 +1,9 @@
-import {useState, useContext} from 'react';
+import {useState, useContext, useEffect} from 'react';
+import ClueInput from '../ClueInput.jsx';
 
 // editor should be true when isSpyMaster and teamTurn matches own team
 const ClueView = ({ teamTurn, editor = false }) => {
-
+  const [editing, setEditing] = useState(editor);
   const [clue, setClue] = useState('waiting for clue...');
   const [clueNumber, setClueNumber] = useState('');
   const socket = useContext(SocketProvider);
@@ -12,16 +13,13 @@ const ClueView = ({ teamTurn, editor = false }) => {
     // be sure to clean up
   }, []);
 
-  { !editor
+  { (!editor && !editing)
     ?
     <div className='clue-container'>
       <div className='clue'>{ clue }</div>
       <div className='number'>{ clueNumber }</div>
     </div>
     :
-    <div className='clue-container'>
-      { // input
-      }
-    </div>
+    <ClueInput />
   }
 }
