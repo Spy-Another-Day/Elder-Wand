08879e5be53c6665b6333fac71e96ffe3318fc88
Shuhua-L <sl.shuhualiu@gmail.com>
2023-06-13 19:56:55 -0400

    fix: merge conflict

--- client/src/Pages/RoomPage.jsx
+++ client/src/Pages/RoomPage.jsx
@@ -1,24 +1,26 @@
-import react, { useEffect, useState, userRef, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import Gameboard from "../Components/Gameboard/Gameboard.jsx";
 import { SocketContext } from "../socket.js";
 import { useParams } from "react-router-dom";
 import Chat from '../components/Chat.jsx';
+import { useUser } from "@clerk/clerk-react";
+
 const RoomPage = () => {
   const socket = useContext(SocketContext);
 
-  const [inGame, setInGame] = useState(true);
+  // 3 possible stages: init, play, result
+  const [gameStage, setGameStage] = useState(true);
   const params = useParams();
+  const { user } = useUser();
 
-  socket.emit("roomID", params);
-  let topic = "general";
+  let topic = "Technology";
   useEffect(() => {
     let temp = {};
     temp.roomID = params.roomID;
     temp.topic = topic;
-    //temp.cards
-    //temp.assassins
-    //temp.agents
-    socket.emit("roomExist", temp);
+    temp.user = user.username;
+    temp.userID = user.id;
+    socket.emit("initRoom", temp);
 
     // axios.get(`${import.meta.env.VITE_SERVER_URL}/initGame/${topic}`)
     // .then(({data}) => {
@@ -30,7 +32,7 @@ const RoomPage = () => {
     // .catch(err=>console.log(err))
   }, []);
 
-  if (inGame) {
+  if (gameStage) {
     return <Gameboard />;
   }
   return (
