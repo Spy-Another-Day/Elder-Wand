332476ac4b0ba281c3ce309b44cf88885ec8e1c9
Shuhua-L <sl.shuhualiu@gmail.com>
2023-06-13 16:12:05 -0400

    chore: save my work

--- client/src/Components/GameSetup.jsx
+++ client/src/Components/GameSetup.jsx
@@ -1,36 +1,53 @@
 import { useEffect, useState, useContext } from "react";
-import { SocketContext } from "../socket.js";
-
 
 const GameSetup = ({ nextStage, gameState }) => {
-  const socket = useContext(SocketContext);
+  // const socket = useContext(SocketContext);
 
-  const [players, setPlayers] = useState({});
-  const [teamMembers, setTeamMembers] = useState(
-    [[], []]);
+  // const [players, setPlayers] = useState({});
+  // const [teamMembers, setTeamMembers] = useState(
+  //   [[], []]);
+
+  // const [assignments, setAssignments] = useState([0, 0]);
 
 
-  useEffect(() => {
-    setPlayers(gameState.players);
-    setTeamMembers([gameState.team_1_members, gameState.team_1_member]);
-    console.log('inside use effect', gameState)
-  }, [gameState])
+  // useEffect(() => {
+  //   setPlayers(gameState.players);
+  //   setTeamMembers([gameState.team_1_members, gameState.team_1_member]);
+  //   console.log('inside use effect', gameState)
+  // }, [gameState])
 
 
   const getTotalPlayers = () => {
-    if (!players) return 0;
-    return Object.values(players)?.length;
+    if (!gameState.players) return 0;
+    return Object.values(gameState.players)?.length;
   }
 
   const getTotalUnassigned = () => {
-    getTotalPlayers - teamMembers[0]?.length - teamMembers[1]?.length;
+    if (!gameState.team_1_member) return 0;
+    return getTotalPlayers - gameState.team_1_member?.length - gameState.team_2_member?.length;
   }
+
+  if (!gameState) {
+    return <div>Loading...</div>
+  }
+
+  console.log(gameState)
 
   return (
     <div className="container flex flex-col h-5/6 max-w-6xl mx-auto my-12 px-8">
       <div className="relative block h-2/3 md:flex items-center space-x-8 my-8">
-        <div className="w-full h-full md:w-1/2 relative rounded bg-neutral shadow-lg overflow-hidden py-8">
-          <h1>Red Team</h1>
+        <div className="relative w-full h-full md:w-1/2 rounded bg-neutral shadow-lg overflow-hidden py-8">
+          <div><h1>Red Team</h1></div>
+          <div className="flex flex-row">
+            <div className="w-1/2">
+              <h3>Spymaster</h3>
+              {gameState.team_1_spymaster}
+            </div>
+            <div className="w-1/2">
+              <h3>Field Operative</h3>
+            </div>
+          </div>
+
 
         </div>
 
@@ -46,7 +63,7 @@ const GameSetup = ({ nextStage, gameState }) => {
           <br />
           waiting on
           <br />
-          {players && Object.values(players).map((player, idx) => {
+          {gameState.players && Object.values(gameState.players).map((player, idx) => {
             console.log('here')
             return (
               <span key={idx}>{player} </span>
@@ -54,7 +71,11 @@ const GameSetup = ({ nextStage, gameState }) => {
           })}
         </div>
       </div>
-      <button onClick={() => nextStage("init")}>Next</button>
+
+      {getTotalUnassigned() === getTotalPlayers() && (
+        <button onClick={() => nextStage("init")}>Start</button>
+      )}
+
     </div>
   );
 }
